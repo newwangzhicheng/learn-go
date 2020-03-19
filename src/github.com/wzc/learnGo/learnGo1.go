81ba3a897df70a52081b62796dbacf0921a7aaad
wzc <newwangzhicheng@hotmail.com>
2020-03-20 00:11:40 +0800

    1

--- src/github.com/wzc/learnGo/learnGo1.go
+++ src/github.com/wzc/learnGo/learnGo1.go
@@ -2,9 +2,31 @@ package main
 
 import "fmt"
 
+const helloWorld = "hello world"
+const holaWorld = "hola world"
+const bonjourWorld = "bonjour world"
+const spanish = "Spanish"
+const french = "French"
+
 //Hello returns a string
-func Hello(name string) string {
-	return "hello world," + name
+func Hello(name string, language string) string {
+	if name == "" {
+		name = "wsq"
+	}
+
+	return greetingprefix(language) + "," + name
+}
+
+func greetingprefix(language string) (prefix string) {
+	switch language {
+	case spanish:
+		prefix = holaWorld
+	case french:
+		prefix = bonjourWorld
+	default:
+		prefix = helloWorld
+	}
+	return
 }
 
 func main() {
--- src/github.com/wzc/learnGo/learnGo1_test.go
+++ src/github.com/wzc/learnGo/learnGo1_test.go
@@ -3,9 +3,28 @@ package main
 import "testing"
 
 func TestHello(t *testing.T) {
-	got := Hello("wzc")
-	want := "hello world,wzc"
-	if got != want {
-		t.Errorf("got %q wat %q", got, want)
+	assertCorrectMessage := func(t *testing.T, got, want string) {
+		t.Helper()
+		if got != want {
+			t.Errorf("got %q want %q", got, want)
+		}
 	}
+
+	t.Run("say hello world to people", func(t *testing.T) {
+		got := Hello("wzc", "")
+		want := "hello world,wzc"
+		assertCorrectMessage(t, got, want)
+	})
+
+	t.Run("say hello world when supply an empty string", func(t *testing.T) {
+		got := Hello("", "")
+		want := "hello world,wsq"
+		assertCorrectMessage(t, got, want)
+	})
+
+	t.Run("in Spanish", func(t *testing.T) {
+		got := Hello("Elodie", "Spanish")
+		want := "hola world,Elodie"
+		assertCorrectMessage(t, got, want)
+	})
 }
