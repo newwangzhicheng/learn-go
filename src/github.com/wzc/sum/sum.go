daff38114fb5c4de936efdad4f786b0168985b2d
wzc <newwangzhicheng@hotmail.com>
2020-03-21 18:40:31 +0800

    3.21

--- src/github.com/wzc/sum/sum.go
+++ src/github.com/wzc/sum/sum.go
@@ -8,3 +8,25 @@ func Sum(numbers []int) int {
 	}
 	return sum
 }
+
+//SumAll got a list of Arrays and returned an Array of their sum
+func SumAll(numbersToSum ...[]int) (sums []int) {
+	for _, numbers := range numbersToSum {
+		sums = append(sums, Sum(numbers))
+	}
+
+	return sums
+}
+
+//SumAllTails got a list of Arrays and returned an Array of their tail sum
+func SumAllTails(numbersToSum ...[]int) (sums []int) {
+	for _, numbers := range numbersToSum {
+		if len(numbers) == 0 {
+			sums = append(sums, 0)
+		} else {
+			tail := numbers[1:]
+			sums = append(sums, Sum(tail))
+		}
+	}
+	return sums
+}
--- src/github.com/wzc/sum/sum_test.go
+++ src/github.com/wzc/sum/sum_test.go
@@ -1,6 +1,9 @@
 package main
 
-import "testing"
+import (
+	"reflect"
+	"testing"
+)
 
 func TestSum(t *testing.T) {
 	assetsString := func(want int, got int, numbers []int) {
@@ -19,4 +22,36 @@ func TestSum(t *testing.T) {
 
 		assetsString(want, got, numbers)
 	})
+}
+
+func TestSumAll(t *testing.T) {
+	got := SumAll([]int{1, 2}, []int{2, 3})
+	want := []int{3, 5}
+
+	if !reflect.DeepEqual(got, want) {
+		t.Errorf("want %v but got %v", want, got)
+	}
+}
+
+func TestSumAllTails(t *testing.T) {
+
+	checkSum := func(t *testing.T, got, want []int) {
+		t.Helper()
+		if !reflect.DeepEqual(got, want) {
+			t.Errorf("want %v but got %v", want, got)
+		}
+	}
+
+	t.Run("make sum of some slices", func(t *testing.T) {
+		got := SumAllTails([]int{1, 2, 3}, []int{2, 3, 4})
+		want := []int{5, 7}
+		checkSum(t, got, want)
+	})
+
+	t.Run("make sum of empty slices", func(t *testing.T) {
+		got := SumAllTails([]int{}, []int{2, 3, 4})
+		want := []int{0, 7}
+		checkSum(t, got, want)
+	})
+
 }
