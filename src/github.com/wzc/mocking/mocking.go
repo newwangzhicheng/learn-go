8c722561c0f75de5b2acfae67f5424fe67f8715b
wzc <newwangzhicheng@hotmail.com>
2020-04-01 22:34:31 +0800

    完成mocking章节

--- src/github.com/wzc/mocking/mocking.go
+++ src/github.com/wzc/mocking/mocking.go
@@ -29,31 +29,45 @@ type CountdownOperationsSpy struct {
 	Calls []string
 }
 
+//ConfigurableSleeper 可配置的sleeper
+type ConfigurableSleeper struct {
+	duration time.Duration
+	sleep    func(time.Duration)
+}
+
+//SpyTime is struct iof duration
+type SpyTime struct {
+	durationSlept time.Duration
+}
+
 func main() {
 	sleeper := &DefaultSleeper{}
 	Countdown(os.Stdout, sleeper)
+
+	configurableSleeper := &ConfigurableSleeper{1 * time.Second, time.Sleep}
+	Countdown(os.Stdout, configurableSleeper)
 }
 
 //Countdown is countdown
 func Countdown(out io.Writer, sleeper Sleeper) {
-	// //Fprint, Fprintf, Fprintln的区别
-	// for i := countdownStart; i > 0; i-- {
-	// 	sleeper.Sleep()
-	// 	fmt.Fprintln(out, i)
-	// }
-
-	// sleeper.Sleep()
-	// fmt.Fprint(out, "Go!")
-
-	for i := countdownStart; i > 0; i-- {
-		fmt.Fprintln(out, i)
-	}
-	fmt.Fprint(out, "Go!")
-
+	//Fprint, Fprintf, Fprintln的区别
 	for i := countdownStart; i > 0; i-- {
 		sleeper.Sleep()
+		fmt.Fprintln(out, i)
 	}
+
 	sleeper.Sleep()
+	fmt.Fprint(out, "Go!")
+
+	// for i := countdownStart; i > 0; i-- {
+	// 	fmt.Fprintln(out, i)
+	// }
+	// fmt.Fprint(out, "Go!")
+
+	// for i := countdownStart; i > 0; i-- {
+	// 	sleeper.Sleep()
+	// }
+	// sleeper.Sleep()
 
 }
 
@@ -68,12 +82,24 @@ func (d *DefaultSleeper) Sleep() {
 }
 
 //Sleep of CountdownOperationsSpy
+//实现了Sleeper接口
 func (s *CountdownOperationsSpy) Sleep() {
 	s.Calls = append(s.Calls, sleep)
 }
 
 //Write of CountdownOperationsSpy
+//实现了io.Writer的接口
 func (s *CountdownOperationsSpy) Write(p []byte) (n int, err error) {
 	s.Calls = append(s.Calls, write)
 	return
+}
+
+//Sleep of SpyTime
+func (s *SpyTime) Sleep(duration time.Duration) {
+	s.durationSlept = duration
+}
+
+//Sleep of ConfigurableSleeper
+func (c *ConfigurableSleeper) Sleep() {
+	c.sleep(c.duration)
 }
--- src/github.com/wzc/mocking/mocking_test.go
+++ src/github.com/wzc/mocking/mocking_test.go
@@ -2,26 +2,63 @@ package main
 
 import (
 	"bytes"
+	"reflect"
 	"testing"
+	"time"
 )
 
 func TestCountdown(t *testing.T) {
-	buffer := &bytes.Buffer{}
-	spySleeper := &SpySleeper{}
+	t.Run("prints 3 to Go", func(t *testing.T) {
+		buffer := &bytes.Buffer{}
+		spySleeper := &SpySleeper{}
 
-	Countdown(buffer, spySleeper)
+		Countdown(buffer, spySleeper)
 
-	got := buffer.String()
-	want := `3
+		got := buffer.String()
+		want := `3
 2
 1
 Go!`
 
-	if got != want {
-		t.Errorf("got '%s' want '%s'", got, want)
-	}
+		if got != want {
+			t.Errorf("got '%s' want '%s'", got, want)
+		}
 
-	if spySleeper.Calls != 4 {
-		t.Errorf("not enough calls to sleeper, want 4 got %d", spySleeper.Calls)
+		if spySleeper.Calls != 4 {
+			t.Errorf("not enough calls to sleeper, want 4 got %d", spySleeper.Calls)
+		}
+	})
+
+	t.Run("sleep before every print", func(t *testing.T) {
+		spySleepPrinter := &CountdownOperationsSpy{}
+		Countdown(spySleepPrinter, spySleepPrinter)
+
+		want := []string{
+			sleep,
+			write,
+			sleep,
+			write,
+			sleep,
+			write,
+			sleep,
+			write,
+		}
+
+		//%v代表相应值的默认格式
+		if !reflect.DeepEqual(want, spySleepPrinter.Calls) {
+			t.Errorf("want %v got %v", want, spySleepPrinter.Calls)
+		}
+
+	})
+}
+
+func TestConfigurableSleeper(t *testing.T) {
+	sleepTime := 3 * time.Second
+	spyTime := &SpyTime{}
+	sleeper := ConfigurableSleeper{sleepTime, spyTime.Sleep}
+	sleeper.Sleep()
+
+	if spyTime.durationSlept != sleepTime {
+		t.Errorf("should have slept for %v but slept for %v", sleepTime, spyTime.durationSlept)
 	}
 }
