20b94c13f99e66e6e3cfe51e911c69085db81b51
wzc <newwangzhicheng@hotmail.com>
2020-04-15 23:05:11 +0800

    reflect第一天的学习

---
+++ src/github.com/wzc/reflection/reflection_test.go
@@ -0,0 +1,49 @@
+package reflection
+
+import (
+	"reflect"
+	"testing"
+)
+
+func TestWalk(t *testing.T) {
+	cases := []struct {
+		Name          string
+		Input         interface{}
+		ExpectedCalls []string
+	}{
+		{
+			"Struct with two string fields",
+			struct {
+				Name string
+				City string
+				Age  int
+			}{"Chris", "Nanjing", 24},
+			[]string{"Chris", "Nanjing"},
+		},
+	}
+
+	for _, test := range cases {
+		t.Run(test.Name, func(t *testing.T) {
+			var got []string
+			walk(test.Input, func(input string) {
+				got = append(got, input)
+			})
+
+			if !reflect.DeepEqual(got, test.ExpectedCalls) {
+				t.Errorf("got %v, want %v", got, test.ExpectedCalls)
+			}
+		})
+	}
+}
+
+func walk(x interface{}, fn func(input string)) {
+	val := reflect.ValueOf(x)
+
+	for i := 0; i < val.NumField(); i++ {
+		field := val.Field(i)
+
+		if field.Kind() == reflect.String {
+			fn(field.String())
+		}
+	}
+}
