151e1d311cac0ec2415056836da4a7bd219893a6
wzc <newwangzhicheng@hotmail.com>
2020-05-25 01:03:50 +0800

    第一天完成啦

--- src/Gee/day1/base1/main.go
+++ src/Gee/day1/base1/main.go
@@ -1,9 +1,31 @@
 /*
  * @Author: wzc
  * @Date: 2020-05-22 01:48:47
- * @LastEditTime: 2020-05-22 02:14:10
+ * @LastEditTime: 2020-05-24 22:26:30
  * @LastEditors: Please set LastEditors
  * @Description: In User Settings Edit
  * @FilePath: \go\src\Gee\day1\base1\main.go
- */ 
- import main
+ */
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+func main() {
+	http.HandleFunc("/", indexHandler)
+	http.HandleFunc("/hello", helloHandler)
+	log.Fatal(http.ListenAndServe(":10200", nil))
+}
+
+func indexHandler(w http.ResponseWriter, req *http.Request) {
+	fmt.Fprintf(w, "URL.Path = %q\n", req.URL.Path)
+}
+
+func helloHandler(w http.ResponseWriter, req *http.Request) {
+	for k, v := range req.Header {
+		fmt.Fprintf(w, "Handler[%q] = %q\n", k, v)
+	}
+}
